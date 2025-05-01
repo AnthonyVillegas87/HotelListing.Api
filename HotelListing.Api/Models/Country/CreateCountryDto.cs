cb1daabb96eca61063b6f0bc8bf06bcfa9d6f302
Anthony Villegas <villegasanthony87@yahoo.com>
2025-05-01 15:23:44 -0500

    fix: refactored POST method w/ DTO (data transfer obj)

---
+++ HotelListing.Api/Models/Country/CreateCountryDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Api.Models.Country;
+
+public class CreateCountryDto
+{
+    [Required]
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+}
